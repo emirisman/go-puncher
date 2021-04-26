984962c2955ed8ea93cf7b8483682765a421f074
Emir Isman <9727151+emirisman@users.noreply.github.com>
2021-04-27 00:48:46 +0200

    switch to TCP

--- server.go
+++ server.go
@@ -5,18 +5,17 @@ import (
 	"net"
 )
 
-var pairs = make(map[string]string)
+var pairs = make(map[string]*net.TCPConn)
 
-// Server --
 func Server() {
 	localAddress := ":8080"
-
-	addr, _ := net.ResolveUDPAddr("udp", localAddress)
-	conn, _ := net.ListenUDP("udp", addr)
+	addr, _ := net.ResolveTCPAddr("tcp", localAddress)
+	list, _ := net.ListenTCP("TCP", addr)
 	fmt.Println("[INFO] Server started")
 	for {
+		conn, _ := list.AcceptTCP()
 		buffer := make([]byte, 1024)
-		bytesRead, remoteAddr, err := conn.ReadFromUDP(buffer)
+		bytesRead, err := conn.Read(buffer)
 		if err != nil {
 			panic(err)
 		}
@@ -24,20 +23,26 @@ func Server() {
 		pairingKey := string(buffer[0:bytesRead])
 		fmt.Println("Received key: ", pairingKey)
 
-		ip1 := remoteAddr.String()
+		ip1 := conn.RemoteAddr().String()
 		ip2 := ""
-		if pairs[pairingKey] == "" {
-			pairs[pairingKey] = ip1
+		if pairs[pairingKey] == nil {
+			pairs[pairingKey] = conn
 			continue
 		} else {
-			ip2 = pairs[pairingKey]
+			conn2 := pairs[pairingKey]
+			conn.Write([]byte(conn2.RemoteAddr().String()))
+			conn2.Write([]byte(conn.RemoteAddr().String()))
+			conn.Close()
+			conn2.Close()
+			pairs[pairingKey] = nil
+			fmt.Printf("[INFO] Key %s paired: %s <-> %s\n", pairingKey, ip1, ip2)
 		}
 
-		r1, _ := net.ResolveUDPAddr("udp", ip1)
-		r2, _ := net.ResolveUDPAddr("udp", ip2)
-		conn.WriteTo([]byte(ip2), r1)
-		conn.WriteTo([]byte(ip1), r2)
-		fmt.Printf("[INFO] Key %s paired: %s <-> %s\n", pairingKey, ip1, ip2)
+		//r1, _ := net.ResolveUDPAddr("udp", ip1)
+		//r2, _ := net.ResolveUDPAddr("udp", ip2)
+		//conn.Write([]byte(ip1))
+		//conn.WriteTo([]byte(ip2), r1)
+		//conn.WriteTo([]byte(ip1), r2)
 
 	}
 }
